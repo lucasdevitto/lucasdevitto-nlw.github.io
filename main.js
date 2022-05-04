967bde1a49f14016e49d78c5dd4160fb2cf05936
Lucasdevitto <79177599+lucasdevitto@users.noreply.github.com>
2022-05-04 11:03:17 -0300

    Stage 2 finalizado (#1)

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function onScroll() {
   scrollY > 0
-    ? (navigation.classList.add('scroll'))
-    : (navigation.classList.remove('scroll'))
+    ? navigation.classList.add('scroll')
+    : navigation.classList.remove('scroll')
 }
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
